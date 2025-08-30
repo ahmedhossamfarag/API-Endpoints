da2c3f0df0a263884dea235985043e2fca15a3f2
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2025-08-30 21:56:16 +0300

    markdown

--- API Endpoints/EndPoint.cs
+++ API Endpoints/EndPoint.cs
@@ -10,16 +10,52 @@ namespace API_Endpoints
 	public class EndPoint
 	{
 		[JsonConverter(typeof(JsonStringEnumConverter))]
-		public RequestType Type {  get; set; }
+		public RequestType Type {  get; set; } = RequestType.GET;
 
-		public string Url { get; set; }
+		public string Url { get; set; } = string.Empty;
 
-		public string Params { get; set; }
+		public string Params { get; set; } = string.Empty;
 
-		public string Body { get; set; }
+		public string Body { get; set; } = string.Empty;
 
-		public string Succes { get; set; }
+		public string Succes { get; set; } = string.Empty;
 
-		public string Error { get; set; }
+		public string Error { get; set; } = string.Empty;
+
+		internal object ToMarkdown()
+		{
+			var b = new StringBuilder();
+			b.AppendLine($"## {Type} [  {Url}  ]\n");
+			if (!string.IsNullOrWhiteSpace(Params) || !string.IsNullOrWhiteSpace(Body))
+			{
+				b.AppendLine("### Request\n");
+				if(!string.IsNullOrWhiteSpace(Params))
+				{
+					b.AppendLine("#### Params\n");
+					b.AppendLine($"```\n{Params}\n```\n");
+				}
+				if(!string.IsNullOrWhiteSpace(Body))
+				{
+					b.AppendLine("#### Body\n");
+					b.AppendLine($"```\n{Body}\n```\n");
+				}
+			}
+			if(!string.IsNullOrWhiteSpace(Succes) || !string.IsNullOrWhiteSpace(Error))
+			{
+				b.AppendLine("### Response\n");
+				if(!string.IsNullOrWhiteSpace(Succes))
+				{
+					b.AppendLine("#### Success\n");
+					b.AppendLine($"```\n{Succes}\n```\n");
+				}
+				if(!string.IsNullOrWhiteSpace(Error))
+				{
+					b.AppendLine("#### Error\n");
+					b.AppendLine($"```\n{Error}```\n");
+				}
+			}
+			b.AppendLine();
+			return b.ToString();
+		}
 	}
 }
--- API Endpoints/MainWindow.xaml.cs
+++ API Endpoints/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace API_Endpoints
 {
@@ -54,17 +44,26 @@ namespace API_Endpoints
 		private void Export_Click(object sender, RoutedEventArgs e)
 		{
 			var dialog = new Microsoft.Win32.SaveFileDialog();
-			dialog.Filter = "Json Files(*.json)|*.json";
+			dialog.Filter = "Json Files(*.json)|*.json|Markdown Files(*.md)|*.md";
 			dialog.DefaultExt = ".json";
+			dialog.AddExtension = true;
 			dialog.Title = "Choose destination file";
 			if (dialog.ShowDialog() == true)
 			{
 				try
 				{
-					var json = JsonSerializer.Serialize(this.Model.EndPoints.ToArray(), Model.SerializerOptions);
+					string text;
+					if (dialog.FileName.EndsWith(".md"))
+					{
+						text = string.Join("\n", Array.ConvertAll(Model.EndPoints.ToArray(), ep => ep.ToMarkdown()));
+					}
+					else
+					{
+						text = JsonSerializer.Serialize(this.Model.EndPoints.ToArray(), Model.SerializerOptions);
+					}
 					using (var file = System.IO.File.CreateText(dialog.FileName))
 					{
-						file.Write(json);
+						file.Write(text);
 					}
 					MessageBox.Show("File Saved");
 				}
@@ -100,6 +99,7 @@ namespace API_Endpoints
 
 		private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
 		{
+			const string indentPattern = "    ";
 			if(sender is TextBox tb)
 			{
 				if(e.Key == Key.OemOpenBrackets && (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
@@ -109,26 +109,36 @@ namespace API_Endpoints
 					tb.CaretIndex = caretIndx + 1;
 					e.Handled = true;
 				}
-				if(e.Key == Key.Enter)
+				else if(e.Key == Key.Tab)
+				{
+					var caretIndx = tb.CaretIndex;
+					tb.Text = tb.Text.Insert(caretIndx, indentPattern);
+					tb.CaretIndex = caretIndx + indentPattern.Length;
+					e.Handled = true;
+				}
+				else if(e.Key == Key.Enter)
 				{
 					var caretIndx = tb.CaretIndex;
 					var textBefore = tb.Text.Substring(0, caretIndx);
 					var textAfter = tb.Text.Substring(caretIndx, tb.Text.Length - caretIndx);
+					var lastLine = textBefore.Split('\n').Last();
+					var indent = "";
+					while (lastLine.StartsWith(indentPattern))
+					{
+						indent += indentPattern;
+						lastLine = lastLine.Substring(indentPattern.Length);
+					}
 					if (textBefore.TrimEnd().EndsWith("{") && textAfter.TrimStart().StartsWith("}"))
 					{
-						var lastLine = textBefore.Split('\n').Last();
-						var indentPattern = "    ";
-						var indent = "";
-						while (lastLine.StartsWith(indentPattern))
-						{
-							indent += indentPattern;
-							lastLine = lastLine.Substring(indentPattern.Length);
-						}
 						tb.Text = tb.Text.Insert(caretIndx, Environment.NewLine + indent + indentPattern + Environment.NewLine + indent);
 						tb.CaretIndex = caretIndx + Environment.NewLine.Length + indent.Length + indentPattern.Length;
-						e.Handled = true;
 					}
-
+					else
+					{
+						tb.Text = tb.Text.Insert(caretIndx, Environment.NewLine + indent);
+						tb.CaretIndex = caretIndx + Environment.NewLine.Length + indent.Length;
+					}
+					e.Handled = true;
 				}
 			}
         }
